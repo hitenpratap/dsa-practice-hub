e106257332fd6089a085aefc68e4b1aef71b6610
Hiten Pratap Singh <hiten.singh@digitalchargingsolutions.com>
2024-06-09 19:24:32 +0200

    feat: Done https://www.hackerrank.com/challenges/compare-two-linked-lists/problem

---
+++ app/src/main/java/dsa/practice/hub/linkedlist/hackerrank/CompareTwiLinkedList.java
@@ -0,0 +1,19 @@
+package dsa.practice.hub.linkedlist.hackerrank;
+
+// https://www.hackerrank.com/challenges/compare-two-linked-lists/problem
+public class CompareTwiLinkedList {
+
+    static boolean compareLists(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        while (head1 != null && head2 != null) {
+            if (head1.data != head2.data) {
+                return false;
+            }
+            head1 = head1.next;
+            head2 = head2.next;
+        }
+        if (head1 != null || head2 != null) {
+            return false;
+        }
+        return true;
+    }
+}
